bfaf2703e13fe1db53e8b6434769982276e730c0
uchiyama777 <ucchi_uchi@hotmail.co.jp>
2020-10-20 16:40:15 +0900

    表示がログイン状態か否かで表示の切り替え

--- app/controllers/furimas_controller.rb
+++ app/controllers/furimas_controller.rb
@@ -1,4 +1,15 @@
 class FurimasController < ApplicationController
+
+  before_action :move_to_index, except: [:index, :show]
+
   def index
   end
+
+ private
+  def move_to_index
+    unless user_signed_in?
+      redirect_to action: :index
+    end
+  end
+
 end
