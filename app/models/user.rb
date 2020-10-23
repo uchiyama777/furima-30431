dc787b18902fb697370ff3a788196fc119444262
uchiyama777 <ucchi_uchi@hotmail.co.jp>
2020-10-23 20:14:02 +0900

    修正

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,21 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+
+  with_options presence: true, format: { with: /\A[ぁ-んァ-ン一-龥]/, message: "is invalid. Input half-width characters."} do
+    validates :first_name
+    validates :last_name
+  end
+    
+  with_options presence: true,format: { with: /\A[ァ-ヶー－]+\z/, message: "is invalid. Input full-width katakana characters."} do
+    validates :first_name_kana
+    validates :last_name_kana
+  end
+  
+  with_options presence: true, format: { with: /\A[a-zA-Z0-9]+\z/, message: "is invalid. Input half-width characters."} do
+    validates :password
+  end
+
+    validates :birth_day, presence: true
+
 end
